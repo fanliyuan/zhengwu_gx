fe89f87e0a28549bb959f77a0625b65d5c6d2980
ChouEric <choueric@foxmail.com>
2018-09-04 17:01:17 +0800

    加入提交前检测代码,prettier

--- src/js/index.1.js
+++ src/js/index.1.js
@@ -1,5 +1,6 @@
 function name(params) {
-  console.log(new Date())
-  console.log(new Date())
+  console.log(new Date());
+  console.log(new Date());
+  console.log(new Date());
 }
-name();;;;
+name();
