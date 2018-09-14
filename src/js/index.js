291a319c0555c33ad2eb7a2549c47cb5931d370a
fanliyuan <fanliyuan2011@163.com>
2018-09-14 15:57:06 +0800

    修改主页

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,20 @@ $(function() {
     // dataType:'jsonp',
     data: { articleCid: 5, pageNum: 1, pageSize: 10 },
     success: function(res) {
+      console.log(res);
       if (+res.code === 0) {
-        // if(res.result.datas.length > 0){
-        //   for(var i=0 ; i< res.result.datas;i++){
-        //     res.result.datas[i]= res.result.datas[i].articleContent.replace(/\bsrc=".*?"/gi,"")
-        //   }
-        // }
-        $("#important_news").html(
-          template("affiche_template", res.result.datas)
-        );
+        if (res.result.datas.length > 0) {
+          console.log(res.result.datas.length);
+          for (var i = 0; i < res.result.datas; i++) {
+            res.result.datas[i].articles = res.result.datas[
+              i
+            ].articleContent.replace(/\bsrc=".*?"[\b|>]/g, "");
+          }
+          console.log(res.result.datas);
+          $("#important_news").html(
+            template("affiche_template", res.result.datas)
+          );
+        }
       }
     },
     error: function(error) {},
