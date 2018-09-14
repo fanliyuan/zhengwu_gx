073e5806f7ca365a2d117c17d468ba0760b9bd71
fanliyuan <fanliyuan2011@163.com>
2018-09-14 13:20:28 +0800

    联调首页

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,11 @@ $(function() {
     data: { articleCid: 5, pageNum: 1, pageSize: 10 },
     success: function(res) {
       if (+res.code === 0) {
-        console.log(res.result.datas);
+        // if(res.result.datas.length > 0){
+        //   for(var i=0 ; i< res.result.datas;i++){
+        //     res.result.datas[i]= res.result.datas[i].articleContent.replace(/\bsrc=".*?"/gi,"")
+        //   }
+        // }
         $("#important_news").html(
           template("affiche_template", res.result.datas)
         );
