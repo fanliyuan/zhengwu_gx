0e8e161ebdd6dca5b7cea238e939b6c45fc3faeb
ChouEric <choueric@foxmail.com>
2018-09-15 18:42:36 +0800

    Revert "首页联调"
    This reverts commit 084f5c21705b57320ccf349e666472787bc59597

--- src/js/index.js
+++ src/js/index.js
@@ -8,67 +8,17 @@ $(function() {
       console.log(res);
       if (+res.code === 0) {
         if (res.result.datas.length > 0) {
-          res.result.datas[0].articleContent = res.result.datas[0].articleContent
-            .replace(/\bsrc=".*?"[\b|>]/g, "")
-            .trim();
-          res.result.datas[0].createOpenTime = moment(
-            +res.result.datas[0].createOpenTime
-          ).format("YYYY-MM-DD");
-          if (res.result.datas[0].articleContent.length > 160) {
-            res.result.datas[0].articleContent =
-              res.result.datas[0].articleContent.substr(0, 160) + "...";
+          console.log(res.result.datas.length);
+          for (var i = 0; i < res.result.datas; i++) {
+            res.result.datas[i].articles = res.result.datas[
+              i
+            ].articleContent.replace(/\bsrc=".*?"[\b|>]/g, "");
           }
-          for (var i = 1; i < res.result.datas.length; i++) {
-            res.result.datas[i].createOpenTime = moment(
-              +res.result.datas[i].createOpenTime
-            ).format("YYYY-MM-DD");
-            if (res.result.datas[i].articleTitle.length > 30) {
-              res.result.datas[i].articleTitle =
-                res.result.datas[i].articleTitle.substr(0, 30).trim() + "...";
-            }
-          }
+          console.log(res.result.datas);
+          $("#important_news").html(
+            template("affiche_template", res.result.datas)
+          );
         }
-        $("#important_news").html(
-          template("affiche_template", res.result.datas)
-        );
-      }
-    },
-    error: function(error) {},
-    timeout: function() {}
-  });
-  $.ajax({
-    type: "get",
-    url: "http://testgoveportal.tpaas.youedata.com/getColumnArtiListById",
-    // dataType:'jsonp',
-    data: { articleCid: 6, pageNum: 1, pageSize: 10 },
-    success: function(res) {
-      console.log(res);
-      if (+res.code === 0) {
-        if (res.result.datas.length > 0) {
-          res.result.datas[0].articleContent = res.result.datas[0].articleContent
-            .replace(/\bsrc=".*?"[\b|>]/g, "")
-            .trim();
-          res.result.datas[0].createOpenTime = moment(
-            +res.result.datas[0].createOpenTime
-          ).format("YYYY-MM-DD");
-          if (res.result.datas[0].articleContent.length > 160) {
-            res.result.datas[0].articleContent =
-              res.result.datas[0].articleContent.substr(0, 160) + "...";
-          }
-          for (var i = 1; i < res.result.datas.length; i++) {
-            res.result.datas[i].createOpenTime = moment(
-              +res.result.datas[i].createOpenTime
-            ).format("YYYY-MM-DD");
-            if (res.result.datas[i].articleTitle.length > 30) {
-              res.result.datas[i].articleTitle =
-                res.result.datas[i].articleTitle.substr(0, 30).trim() + "...";
-            }
-          }
-        }
-        // $("#important_news").html(
-        //   template("affiche_template", res.result.datas)
-        // );
-        $("#affiche").html(template("affiche_template", res.result.datas));
       }
     },
     error: function(error) {},
