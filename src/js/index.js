e32777a21dacd381b87c0df218a9f758b964394b
fanliyuan <fanliyuan2011@163.com>
2018-09-20 18:17:48 +0800

    联调目录管理的模块

--- src/js/index.js
+++ src/js/index.js
@@ -154,4 +154,33 @@ $(function() {
       console.log("连接超时");
     }
   });
+  $.ajax({
+    type: "get",
+    url: "http://192.168.100.16:8809/directoryOpenList",
+    success: function(res) {
+      if (+res.code === 0) {
+        if (res.result.datas.length > 16) {
+          res.result.datas = res.result.datas.slice(0, 16);
+        }
+        // var queryData = {}
+        $("#catalog").html(template("catalog_list", res.result.datas));
+        $("#catalog")
+          .children("ul")
+          .on("click", "li", function() {
+            // queryData.catalogId = $(this).data('typeId')
+            $(this)
+              .parent()
+              .children(".click")
+              .removeClass("click");
+            $(this).addClass("click");
+          });
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    },
+    timeout: function() {
+      console.log("连接超时");
+    }
+  });
 });
