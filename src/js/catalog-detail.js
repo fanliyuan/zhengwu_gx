dea67ac9791d41c9eaea14cab0094a25b34670af
fanliyuan <fanliyuan2011@163.com>
2018-09-27 13:34:40 +0800

    修改资源目录详情页bug2

--- src/js/catalog-detail.js
+++ src/js/catalog-detail.js
@@ -2,7 +2,7 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2018-09-26 17:24:15 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-09-27 13:30:39
+ * @Last Modified time: 2018-09-27 13:33:49
  */
 $(function() {
   function getUrlParam(name) {
@@ -54,8 +54,9 @@ $(function() {
     data: { id: "60f68572f608491896959e720c289eff" },
     success: function(res) {
       if (+res.code === 200) {
+        var cs = res.data.name ? res.data.name : "jdbc-20";
         $("#detail .info").on("click", ".downloadBtn", function() {
-          var hrefs = "http://cdyoue.com.cn:19081/connector/" + res.data.name;
+          var hrefs = "http://cdyoue.com.cn:19081/connector/" + cs;
           // window.open(hrefs);
           window.location = hrefs;
           window.location.href = hrefs;
